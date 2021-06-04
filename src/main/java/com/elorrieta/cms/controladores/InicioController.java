0494ed9e6db307cfe4df89472f30da7064ebb108
ander.uraga@gmail.com <ander.uraga@gmail.com>
2021-06-04 09:05:41 +0200

    web.xml o descriptor del proyecto web

---
+++ src/main/java/com/elorrieta/cms/controladores/InicioController.java
@@ -0,0 +1,46 @@
+package com.elorrieta.cms.controladores;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InicioController
+ */
+@WebServlet("/inicio")
+public class InicioController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		System.out.println("Esto es lo primero que se ejecuta");
+
+		// llamar DAO para recuperar datos
+
+		// enviar como atributos
+
+		// ir a la vista
+
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+
+	}
+
+}
